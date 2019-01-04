58b818f891adf90ec57c96e66387ff6f82c4be29
zhouyswo <zhouyswo@163.com>
2019-01-04 10:46:01 +0800

    更新目录结构

--- src/java/com/neural/network/ongpu/Lenet5.java
+++ src/java/com/neural/network/ongpu/Lenet5.java
@@ -1,8 +1,17 @@
 package com.neural.network.ongpu;
 
+import org.nd4j.linalg.api.buffer.DataBuffer;
+import org.nd4j.linalg.factory.Nd4j;
+
 /**
  * Created by DELL on 2019/1/4.
  */
 public class Lenet5 {
+    public static void main(String args[]){
+
+    }
+    public void lenet5(){
+        Nd4j.setDataType(DataBuffer.Type.HALF);
+    }
 
 }
