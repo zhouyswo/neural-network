cbc3028057085ecf1d70c2f256dccea4728fdef4
zhouyswo <zhouyswo@163.com>
2019-01-04 13:44:27 +0800

    更新目录结构

--- src/java/com/neural/network/oncpu/Lenet5.java
+++ src/java/com/neural/network/oncpu/Lenet5.java
@@ -4,5 +4,12 @@ package com.neural.network.oncpu;
  * Created by DELL on 2019/1/4.
  */
 public class Lenet5 {
+    public static void main(String args[]) {
+
+    }
+
+    public void Lenet5() {
+
+    }
 
 }
