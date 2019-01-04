4ea44ca2c47d8ad8ee73558dc2c06ff57612f63c
zhouyswo <zhouyswo@163.com>
2019-01-04 12:23:24 +0800

    更新目录结构

--- src/java/com/neural/network/ongpu/MutilGpuLenet5.java
+++ src/java/com/neural/network/ongpu/MutilGpuLenet5.java
@@ -1,5 +1,6 @@
 package com.neural.network.ongpu;
 
+import org.nd4j.jita.conf.CudaEnvironment;
 import org.nd4j.linalg.api.buffer.DataBuffer;
 import org.nd4j.linalg.factory.Nd4j;
 
@@ -7,11 +8,27 @@ import org.nd4j.linalg.factory.Nd4j;
  * Created by DELL on 2019/1/4.
  */
 public class MutilGpuLenet5 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
     }
-    public void lenet5(){
+
+    public void lenet5() {
         Nd4j.setDataType(DataBuffer.Type.HALF);
+        CudaEnvironment.getInstance()
+                .getConfiguration()
+                .allowMultiGPU(true)
+                .setMaximumDeviceCache(2 * 1024 * 1024 * 1024L)
+                .allowCrossDeviceAccess(true);
+        //图像通道数
+        int imgChannels = 1;
+        //输出类别数
+        int outLabels = 10;
+        //每一批次处理的数据量
+        int batchSize = 128;
+        //迭代轮数
+        int epochs = 10;
+        //随机种子
+        int seed = 64;
     }
 
 }
